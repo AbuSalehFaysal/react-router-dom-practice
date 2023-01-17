21643c20517b487ef198ff353e06d637bdc92da6
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2023-01-17 22:30:51 +0600

    navigate programmatically

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div>Home</div>
+      <button onClick={() => navigate("order-summary", { replace: true })}>
+        Place Order
+      </button>
+    </div>
+  );
 };
 
 export default Home;
