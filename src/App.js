de5ba6b26d22b2f3f912c119e13e23ae3af2a16d
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2023-01-17 22:34:39 +0600

    no match route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import OrderSummary from "./components/OrderSummary";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/order-summary" element={<OrderSummary />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="order-summary" element={<OrderSummary />}></Route>
+        <Route path="*" element={<NoMatch />}></Route>
       </Routes>
     </>
   );
