6a3abb24e2eaa5731952facf32e3a216e646387a
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2023-01-31 01:28:32 +0600

    protected route

---
+++ src/components/Login.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "./auth";
+
+const Login = () => {
+  const [user, setUser] = useState("");
+
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const redirectPath = location.state?.path || "/";
+
+  const handleLogin = () => {
+    auth.login(user);
+    navigate(redirectPath, { replace: true });
+  };
+  return (
+    <div>
+      <label htmlFor="">
+        UserName:{" "}
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={(e) => setUser(e.target.value)}
+        />
+      </label>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
